20bed8d2b9ccaf1ed7cb9ca6d6e6f19f4dfa4477
Sbroul12 <bhusans271@gmail.com>
2024-08-25 21:32:45 +0530

    Your commit messageUpdated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sashi <code>src/App.js</code> and save to reload.
+          Sashi <code>Bhusan</code> and save to reload.
         </p>
         <a
           className="App-link"
